35a0c3f04bd64e05d160657ae4a6f289cf91c60c
Cynto <lucagaravello@outlook.com>
2023-02-07 18:18:55 +0100

    Update user-service functions

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,5 +1,5 @@
 import userRepo from '@src/repos/user-repo';
-import { IUser } from '@src/models/User';
+import { User } from '@src/models/User';
 import { RouteError } from '@src/declarations/classes';
 import HttpStatusCodes from '@src/declarations/major/HttpStatusCodes';
 
@@ -12,22 +12,40 @@ export const userNotFoundErr = 'User not found';
 /**
  * Get all users.
  */
-function getAll(): Promise<IUser[]> {}
+function getAll(): Promise<User[] | null | void> {
+  return userRepo.getAll();
+}
 
 /**
  * Add one user.
  */
-function addOne(user: IUser): Promise<void> {}
+function addOne(user: User): Promise<void> {
+  return userRepo.add(user);
+}
 
 /**
  * Update one user.
  */
-async function updateOne(user: IUser): Promise<void> {}
+async function updateOne(user: User): Promise<void> {
+  const persists = await userRepo.persists(user.user_id);
+  if (!persists) {
+    throw new RouteError(HttpStatusCodes.NOT_FOUND, userNotFoundErr);
+  }
+  // Return user
+  return userRepo.update(user);
+}
 
 /**
  * Delete a user by their id.
  */
-async function _delete(id: number): Promise<void> {}
+async function _delete(user_id: string): Promise<void> {
+  const persists = await userRepo.persists(user_id);
+  if (!persists) {
+    throw new RouteError(HttpStatusCodes.NOT_FOUND, userNotFoundErr);
+  }
+  // Delete user
+  return userRepo.delete(user_id);
+}
 
 // **** Export default **** //
 
