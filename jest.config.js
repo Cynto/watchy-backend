3cbeb238524ad6fc39505ac76e36b00cbe5a51cf
Cynto <lucagaravello@outlook.com>
2023-06-14 17:03:17 +0200

    Remove setup file which isn't needed

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['<rootDir>/setEnvVars.ts'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1',
