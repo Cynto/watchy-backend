d9eb1871b189eb8d9033b02afc1f3cd4a6330c05
Cynto <lucagaravello@outlook.com>
2023-06-21 17:39:13 +0200

    Add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -13,16 +14,22 @@ import HttpStatusCodes from '@src/declarations/major/HttpStatusCodes';
 import { NodeEnvs } from '@src/declarations/enums';
 import { RouteError } from '@src/declarations/classes';
 import '@src/config/database';
-import '@src/models/index';
+import createAllTables from '@src/models';
+createAllTables();
 
 dotenv.config();
-
 // **** Init express **** //
 
 const app = express();
 
+// *** Create allowed origins for CORS *** //
+const allowedOrigins = ['http://localhost'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 // **** Set basic express settings **** //
-
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(EnvVars.cookieProps.secret));
