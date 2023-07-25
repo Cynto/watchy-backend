8a117b384b2829c531a0dfe315130a30a727953b
Cynto <lucagaravello@outlook.com>
2023-07-25 17:01:33 +0200

    Add regex checks to password and username. Add * to start of each error message

--- src/validators/user-validators.ts
+++ src/validators/user-validators.ts
@@ -9,24 +9,40 @@ interface ReqBody {
 
 export const validateUserCreation = [
   body('username')
-    .isLength({ min: 3, max: 20 })
-    .withMessage('Username must contain between 3 to 20 characters.')
     .trim()
+    .isLength({ min: 3, max: 16 })
+    .withMessage('*Username must contain between 3 to 16 characters.')
+    .matches(/^(?=.*[a-zA-Z])[a-zA-Z0-9_-]+$/)
+    .withMessage(
+      '*Usernames must include at least one letter and can contain numbers, hyphens, and underscores.',
+    )
     .escape(),
-  body('email').isEmail().withMessage('Email must be valid.').normalizeEmail(),
+  body('email')
+    .isEmail()
+    .withMessage('*Please enter a valid email.')
+    .normalizeEmail(),
   body('password')
-    .isLength({ min: 8 })
-    .withMessage('Password must include at least 8 characters.')
     .trim()
+    .isLength({ min: 8 })
+    .withMessage('*Password must contain at least 8 characters.')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&;:])[A-Za-z\d@$!%*?&;:]{8,}$/,
+    )
+    .withMessage(
+      '*Password must be at least 8 characters long and contain at least one alphabetic character, one digit, and one special character from the set @$!%*#?&;',
+    )
     .escape(),
   body('passwordConfirm')
+    .trim()
     .custom((value, { req }) => {
       const rBody = req.body as ReqBody;
       if (value !== rBody.password) {
-        throw new Error('Passwords do not match.');
+        throw new Error(
+          '*Password confirmation does not match the original password. Please ensure both passwords are the same.',
+        );
       }
       return true;
     })
-    .trim()
+
     .escape(),
 ];
