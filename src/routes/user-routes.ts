11f65c2ac2eeab24653da4bfc614dc6104dd2193
Cynto <lucagaravello@outlook.com>
2023-07-25 16:59:49 +0200

    Properly implement add user route. If errors occur in validation, or through query, send res with errors and error code. Otherwise, return created error code

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -7,6 +7,7 @@ import { validationResult } from 'express-validator';
 import httpStatusCodes from '@src/declarations/major/HttpStatusCodes';
 import pwdUtil from '@src/util/pwd-util';
 import UserF from '@src/models/User';
+import { PgError } from '@src/types/pg';
 
 // **** Variables **** //
 
@@ -29,16 +30,21 @@ async function getAll(_: IReq, res: IRes) {
   return res.status(HttpStatusCodes.OK).json({ users });
 }
 
-/**
- * Add one user.
- */
+// This function handles the addition of a new user to the system.
+// It expects an HTTP request (req) containing the user information and
+// an HTTP response (res) to send back the result of the operation.
+
 async function add(req: IReq<EditableUser>, res: IRes) {
+  // Validate the request data using the express validator validationResult function.
+  // If there are validation errors, log them and return a Bad Request response with the errors.
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    return res.status(httpStatusCodes.BAD_REQUEST).end();
+    return res.status(httpStatusCodes.BAD_REQUEST).json(errors).end();
   }
 
+  // Create a new User object based on the request body data.
+  // The password is hashed using the pwdUtil.getHash function, which presumably provides a secure hash.
   const user = UserF.new(
     req.body.username,
     req.body.email,
@@ -46,8 +52,58 @@ async function add(req: IReq<EditableUser>, res: IRes) {
     undefined,
     await pwdUtil.getHash(req.body.password),
   );
-  await userService.addOne(user);
-  return res.status(HttpStatusCodes.CREATED).end();
+
+  // Attempt to add the user to the database using the userService.addOne function.
+  // The result of the operation is stored in the queryRes variable.
+  const queryRes = await userService.addOne(user);
+
+  // If the queryRes is true, it means the user was successfully added.
+  // Return a response with the HTTP status code indicating successful creation.
+  if (queryRes === true) {
+    return res.status(HttpStatusCodes.CREATED).end();
+  } else {
+    // If there was an error while adding the user, handle it here.
+
+    if (queryRes instanceof Error && 'code' in queryRes) {
+      const queryErr = queryRes as PgError;
+      // Check if the error is a unique constraint violation (23505 error code).
+      // If the unique constraint violation is for the 'username' field, return a Conflict response
+      // with a specific error message for the username field.
+      if (queryErr.code === '23505' && queryErr.detail.includes('username')) {
+        return res
+          .status(httpStatusCodes.CONFLICT)
+          .json({
+            status: 409,
+            errors: [
+              {
+                type: 'field',
+                msg: '*This username is unavailable.',
+                path: 'username',
+                location: 'body',
+              },
+            ],
+          })
+          .end();
+      } else {
+        // If the unique constraint violation is for the 'email' field, return a Conflict response
+        // with a specific error message for the email field.
+        return res
+          .status(httpStatusCodes.CONFLICT)
+          .json({
+            status: 409,
+            errors: [
+              {
+                type: 'field',
+                msg: '*Please use a different email.',
+                path: 'email',
+                location: 'body',
+              },
+            ],
+          })
+          .end();
+      }
+    }
+  }
 }
 
 /**
