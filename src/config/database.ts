16d0aa627bfef0f894cacc16f1dbde2f5173bb93
Cynto <lucagaravello@outlook.com>
2023-07-31 17:34:23 +0200

    Create createDatabase function to create the watchy database if it doesn't exist. Create initializeDB function that connects to db, and then if successful, creates db tables

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,26 +1,17 @@
-import { Pool, QueryResult } from 'pg';
 import '../pre-start'; // Must be the first import
+import { Pool, QueryResult } from 'pg';
 import logger from 'jet-logger';
 import EnvVars from '@src/declarations/major/EnvVars';
+import createAllTables from '@src/models';
 
-const pool = new Pool({
+let poolConfig = {
   host: EnvVars.db.host,
-  database: EnvVars.db.database,
+  database: 'postgres', // Connect to the 'postgres' database initially
   user: EnvVars.db.user,
   password: EnvVars.db.password,
-});
-
-const connectToDB = async () => {
-  try {
-    await pool.connect();
-    logger.info(
-      `Connection to Postgres ${EnvVars.db.database} database was successful`,
-    );
-  } catch (err) {
-    logger.err(`Connection to Postgres ${EnvVars.db.database} database failed`);
-  }
 };
-connectToDB();
+
+let pool = new Pool(poolConfig);
 
 const db = {
   query: async (text: string, params: unknown[]): Promise<QueryResult> => {
@@ -33,4 +24,64 @@ const db = {
     return res;
   },
 };
-export { db };
+
+const createDatabase = async (dbName: string) => {
+  try {
+    // Connect to the 'postgres' database to issue the CREATE DATABASE command
+    const client = await pool.connect();
+    const createDbQuery = `CREATE DATABASE ${dbName}`;
+    await client.query(createDbQuery);
+    client.release();
+    logger.info(`Database '${dbName}' created successfully.`);
+  } catch (error) {
+    logger.err(error);
+    throw error;
+  }
+};
+
+const connectToDB = async () => {
+  try {
+    const dbName = EnvVars.db.database;
+
+    // Check if the database exists
+    const checkDbQuery = `SELECT 1 FROM pg_database WHERE datname = $1`;
+    const values = [dbName];
+    const result = await pool.query(checkDbQuery, values);
+    const exists = result.rowCount > 0;
+
+    if (!exists) {
+      // Create the database if it doesn't exist
+      await createDatabase(dbName);
+    }
+    // Update the pool's config to use the newly created database
+    poolConfig = {
+      ...poolConfig,
+      database: dbName,
+    };
+
+    // End the old pool before creating the new one
+    await pool.end();
+    // Reassign the pool with the updated configuration
+    pool = new Pool(poolConfig);
+
+    await pool.connect();
+    logger.info(`Connection to Postgres '${dbName}' database was successful.`);
+  } catch (err) {
+    logger.err(
+      `Connection to Postgres '${EnvVars.db.database}' database failed.`,
+    );
+    throw err;
+  }
+};
+const initializeDB = async () => {
+  try {
+    await connectToDB();
+    await createAllTables();
+  } catch (e) {
+    logger.err(e);
+    throw e;
+  }
+};
+initializeDB();
+
+export { db }; // Export the updated db object
