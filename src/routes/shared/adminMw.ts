72de871d097501c36ebc4a72cfd11cc0db8bee99
Cynto <lucagaravello@outlook.com>
2023-08-08 15:44:01 +0200

    Replace IUser with User interface

--- src/routes/shared/adminMw.ts
+++ src/routes/shared/adminMw.ts
@@ -8,15 +8,11 @@ import { JwtPayload } from 'jsonwebtoken';
 import HttpStatusCodes from '@src/declarations/major/HttpStatusCodes';
 import EnvVars from '@src/declarations/major/EnvVars';
 import jwtUtil from '@src/util/jwt-util';
-import { IUser, UserRoles } from '@src/models/User';
-
+import { User } from '@src/models/User';
 
 // **** Variables **** //
-
 const jwtNotPresentErr = 'JWT not present in signed cookie.',
   userUnauthErr = 'User not authorized to perform this action';
-
-
 
 // **** Types **** //
 
@@ -24,44 +20,36 @@ export interface ISessionUser extends JwtPayload {
   id: number;
   email: string;
   name: string;
-  role: IUser['role'];
+  rank: User['rank'];
 }
-
 
 // **** Functions **** //
 
 /**
  * See note at beginning of file.
  */
-async function adminMw(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+async function adminMw(req: Request, res: Response, next: NextFunction) {
   // Extract the token
   const cookieName = EnvVars.cookieProps.key,
     jwt = req.signedCookies[cookieName];
   if (!jwt) {
+    console.log(jwt);
     return res
       .status(HttpStatusCodes.UNAUTHORIZED)
       .json({ error: jwtNotPresentErr });
   }
   // Make sure user role is an admin
   const clientData = await jwtUtil.decode<ISessionUser>(jwt);
-  if (
-    typeof clientData === 'object' &&
-    clientData.role === UserRoles.Admin
-  ) {
+  if (typeof clientData === 'object' && clientData.rank >= 3) {
     res.locals.sessionUser = clientData;
     return next();
-  // Return an unauth error if user is not an admin
+    // Return an unauth error if user is not an admin
   } else {
     return res
       .status(HttpStatusCodes.UNAUTHORIZED)
       .json({ error: userUnauthErr });
   }
 }
-
 
 // **** Export Default **** //
 
